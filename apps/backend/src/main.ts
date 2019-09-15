bc169d33a8ff2d40bbdea8607389025515de774c
Gui Seek <guiseek@gmail.com>
2019-09-15 20:34:58 -0300

    swagger ok

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,12 +7,26 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'api';
-  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(globalPrefix);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Realworld Example App')
+    .setDescription('The Realworld API description')
+    .setVersion('1.0')
+    .setBasePath(globalPrefix)
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
+
   const port = process.env.port || 3333;
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
--- libs/auth/backend/users/src/dto/user-signup.dto.ts
+++ libs/auth/backend/users/src/dto/user-signup.dto.ts
@@ -1,13 +1,17 @@
 import { IsNotEmpty, IsEmail } from 'class-validator/decorator/decorators';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class UserSignupDto {
+  @ApiModelProperty()
   @IsNotEmpty()
   readonly username: string;
 
+  @ApiModelProperty()
   @IsNotEmpty({ message: 'Email obrigatório' })
   @IsEmail({ require_tld: true }, { message: 'Email inválido' })
   readonly email: string;
 
+  @ApiModelProperty()
   @IsNotEmpty()
   readonly password: string;
 }
