cbb2a9bea408d93542209a12032874fd3e956dab
Gui Seek <guiseek@gmail.com>
2019-10-03 01:07:26 -0300

    clean

--- libs/account-ui/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'account-ui',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/account-ui',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
